652a526a36b5bd7c3a741b6e755852b8fd5b89e4
Lloyd Lucas <37113984+lloydlucas@users.noreply.github.com>
2024-05-28 14:43:51 -0400

    Update intune-installer-ubuntu22.04_amd64.sh

--- intune-installer-ubuntu22.04_amd64.sh
+++ intune-installer-ubuntu22.04_amd64.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+echo "Installing curl"
+sudo apt update && sudo apt install -y curl
 
+echo "Adding repo for Microsoft Edge and Intune Portal..."
 # Microsoft Edge Repos https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/
-echo "Downloading Microsoft Edge and Intune Portal..."
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/ 
+sudo install -o root -g root -m 644 /tmp/microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+sudo install -o root -g root -m 644 /tmp/microsoft.gpg /usr/share/keyrings/ 
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/ubuntu/22.04/prod jammy main" > /etc/apt/sources.list.d/microsoft-ubuntu-jammy-prod.list' 
 
 echo "Installing Microsoft Edge and Intune Portal..."
